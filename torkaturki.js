aee917bcef61c1fb0119c846bc7a78082e9676f1
tamim <abutalhatamim017@gmail.com>
2024-10-01 00:36:01 +0600

    my work has been finest

---
+++ torkaturki.js
@@ -0,0 +1,12 @@
+function sum(a,s,d,){
+    const result = a + s + d;
+    return result;
+}
+
+const totale = sum( 45, 89, 65);
+console.log(totale);
+
+const arr1 = [1, 2, 3];
+const arr2 = arr1;
+arr1.push(50);
+console.log(arr2);
